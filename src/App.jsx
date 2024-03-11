9fa44b4a7a34ff4e9a1134f3ba3d17c60e07fe77
SIMPLY-CHAOTIC <jithinbaby5@gmail.com>
2024-03-11 15:06:32 +0530

    introduction codes to react including props

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Student from './students'
+
+
+function App() {
+ 
+   return(
+      <>
+            <Student name="jithin" age={22} isStudent={true}/>
+            <Student name="george" age={22} isStudent={true}/>
+            <Student name="bimal" age={22} isStudent={true}/>
+            <Student/>
+    
+     </>
+
+   );
+   
+  
+}
+
+export default App
+
+
